3a791bc72b97c4759129005c0260fd12eca87be9
rsandersjr <rsandersjr@gmail.com>
2022-12-27 12:19:34 -0500

    Update spellcraft.lua
    Updated Gem names to match Edens new name. I don't have Alb/Hib SC so those names will need to be double checked/and some updates.

--- crafter/profiles/eden/spellcraft.lua
+++ crafter/profiles/eden/spellcraft.lua
@@ -4,253 +4,253 @@ return T {
 		--alb
 		--tier 1
 		T{
-			T{level = 19, name = 'raw Earthen Essence Jewel',},
+			T{level = 19, name = 'raw Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'raw Earthen War Sigil',},
-			T{level = 65, name = 'raw Blood Essence Jewel',},
+			T{level = 65, name = 'raw Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'raw Airy Fervor Sigil',},
-			T{level = 99, name = 'raw Earthen Shielding Jewel',},
+			T{level = 99, name = 'raw Earthen Shielding Jewel (Matter Resist)',},
 			
 		},
 		--Tier 2
 		T{
-			T{level = 19, name = 'uncut Earthen Essence Jewel',},
+			T{level = 19, name = 'uncut Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'uncut Earthen War Sigil',},
-			T{level = 65, name = 'uncut Blood Essence Jewel',},
+			T{level = 65, name = 'uncut Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'uncut Airy Fervor Sigil',},
-			T{level = 99, name = 'uncut Earthen Shielding Jewel',},
+			T{level = 99, name = 'uncut Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 3
 		T{
-			T{level = 19, name = 'rough Earthen Essence Jewel',},
+			T{level = 19, name = 'rough Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'rough Earthen War Sigil',},
-			T{level = 65, name = 'rough Blood Essence Jewel',},
+			T{level = 65, name = 'rough Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'rough Airy Fervor Sigil',},
-			T{level = 99, name = 'rough Earthen Shielding Jewel',},
+			T{level = 99, name = 'rough Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 4
 		T{
-			T{level = 19, name = 'Flawed Earthen Essence Jewel',},
+			T{level = 19, name = 'Flawed Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'Flawed Earthen War Sigil',},
-			T{level = 65, name = 'Flawed Blood Essence Jewel',},
+			T{level = 65, name = 'Flawed Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'Flawed Airy Fervor Sigil',},
-			T{level = 99, name = 'Flawed Earthen Shielding Jewel',},
+			T{level = 99, name = 'Flawed Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 5
 		T{
-			T{level = 19, name = 'imperfect Earthen Essence Jewel',},
+			T{level = 19, name = 'imperfect Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'imperfect Earthen War Sigil',},
-			T{level = 65, name = 'imperfect Blood Essence Jewel',},
+			T{level = 65, name = 'imperfect Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'imperfect Airy Fervor Sigil',},
-			T{level = 99, name = 'imperfect Earthen Shielding Jewel',},
+			T{level = 99, name = 'imperfect Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 6
 		T{
-			T{level = 19, name = 'polished Earthen Essence Jewel',},
+			T{level = 19, name = 'polished Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'polished Earthen War Sigil',},
-			T{level = 65, name = 'polished Blood Essence Jewel',},
+			T{level = 65, name = 'polished Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'polished Airy Fervor Sigil',},
-			T{level = 99, name = 'polished Earthen Shielding Jewel',},
+			T{level = 99, name = 'polished Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 7
 		T{
-			T{level = 19, name = 'faceted Earthen Essence Jewel',},
+			T{level = 19, name = 'faceted Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'faceted Earthen War Sigil',},
-			T{level = 65, name = 'faceted Blood Essence Jewel',},
+			T{level = 65, name = 'faceted Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'faceted Airy Fervor Sigil',},
-			T{level = 99, name = 'faceted Earthen Shielding Jewel',},
+			T{level = 99, name = 'faceted Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 8
 		T{
-			T{level = 19, name = 'precious Earthen Essence Jewel',},
+			T{level = 19, name = 'precious Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'precious Earthen War Sigil',},
-			T{level = 65, name = 'precious Blood Essence Jewel',},
+			T{level = 65, name = 'precious Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'precious Airy Fervor Sigil',},
-			T{level = 99, name = 'precious Earthen Shielding Jewel',},
+			T{level = 99, name = 'precious Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 9
 		T{
-			T{level = 19, name = 'flawless Earthen Essence Jewel',},
+			T{level = 19, name = 'flawless Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'flawless Earthen War Sigil',},
-			T{level = 65, name = 'flawless Blood Essence Jewel',},
+			T{level = 65, name = 'flawless Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'flawless Airy Fervor Sigil',},
-			T{level = 99, name = 'flawless Earthen Shielding Jewel',},
+			T{level = 99, name = 'flawless Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 10
 		T{
-			T{level = 19, name = 'perfect Earthen Essence Jewel',},
+			T{level = 19, name = 'perfect Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'perfect Earthen War Sigil',},
-			T{level = 65, name = 'perfect Blood Essence Jewel',},
+			T{level = 65, name = 'perfect Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'perfect Airy Fervor Sigil',},
-			T{level = 99, name = 'perfect Earthen Shielding Jewel',},
+			T{level = 99, name = 'perfect Earthen Shielding Jewel (Matter Resist)',},
 		},
 	},
 	T{
 		--mid
 		--tier 1
 		T{
-			T{level = 19, name = 'raw Earthen Essence Jewel',},
-			T{level = 41, name = 'raw Earthen War Rune Stone',},
-			T{level = 65, name = 'raw Blood Essence Jewel',},
-			T{level = 84, name = 'raw Earthern Primal Rune Stone',},
-			T{level = 99, name = 'raw Earthen Shielding Jewel',},
+			T{level = 19, name = 'raw Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'raw Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'raw Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'raw Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'raw Earthen Shielding Jewel (Matter Resist)',},
 			
 		},
 		--Tier 2
 		T{
-			T{level = 19, name = 'uncut Earthen Essence Jewel',},
-			T{level = 41, name = 'uncut Earthen War Rune Stone',},
-			T{level = 65, name = 'uncut Blood Essence Jewel',},
-			T{level = 84, name = 'uncut Earthern Primal Rune Stone',},
-			T{level = 99, name = 'uncut Earthen Shielding Jewel',},
+			T{level = 19, name = 'uncut Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'uncut Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'uncut Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'uncut Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'uncut Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 3
 		T{
-			T{level = 19, name = 'rough Earthen Essence Jewel',},
-			T{level = 41, name = 'rough Earthen War Rune Stone',},
-			T{level = 65, name = 'rough Blood Essence Jewel',},
-			T{level = 84, name = 'rough Earthern Primal Rune Stone',},
-			T{level = 99, name = 'rough Earthen Shielding Jewel',},
+			T{level = 19, name = 'rough Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'rough Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'rough Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'rough Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'rough Earthen Shielding Jewel (Matter Resist)',},
 
 		},
 		--Tier 4
 		T{
-			T{level = 19, name = 'Flawed Earthen Essence Jewel',},
-			T{level = 41, name = 'Flawed Earthen War Rune Stone',},
-			T{level = 65, name = 'Flawed Blood Essence Jewel',},
-			T{level = 84, name = 'Flawed Earthern Primal Rune Stone',},
-			T{level = 99, name = 'Flawed Earthen Shielding Jewel',},
+			T{level = 19, name = 'Flawed Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'Flawed Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'Flawed Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'Flawed Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'Flawed Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 5
 		T{
-			T{level = 19, name = 'imperfect Earthen Essence Jewel',},
-			T{level = 41, name = 'imperfect Earthen War Rune Stone',},
-			T{level = 65, name = 'imperfect Blood Essence Jewel',},
-			T{level = 84, name = 'imperfect Earthern Primal Rune Stone',},
-			T{level = 99, name = 'imperfect Earthen Shielding Jewel',},
+			T{level = 19, name = 'imperfect Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'imperfect Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'imperfect Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'imperfect Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'imperfect Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 6
 		T{
-			T{level = 19, name = 'polished Earthen Essence Jewel',},
-			T{level = 41, name = 'polished Earthen War Rune Stone',},
-			T{level = 65, name = 'polished Blood Essence Jewel',},
-			T{level = 84, name = 'polished Earthern Primal Rune Stone',},
-			T{level = 99, name = 'polished Earthen Shielding Jewel',},
+			T{level = 19, name = 'polished Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'polished Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'polished Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'polished Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'polished Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 7
 		T{
-			T{level = 19, name = 'faceted Earthen Essence Jewel',},
-			T{level = 41, name = 'faceted Earthen War Rune Stone',},
-			T{level = 65, name = 'faceted Blood Essence Jewel',},
-			T{level = 84, name = 'faceted Earthern Primal Rune Stone',},
-			T{level = 99, name = 'faceted Earthen Shielding Jewel',},
+			T{level = 19, name = 'faceted Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'faceted Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'faceted Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'faceted Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'faceted Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 8
 		T{
-			T{level = 19, name = 'precious Earthen Essence Jewel',},
-			T{level = 41, name = 'precious Earthen War Rune Stone',},
-			T{level = 65, name = 'precious Blood Essence Jewel',},
-			T{level = 84, name = 'precious Earthern Primal Rune Stone',},
-			T{level = 99, name = 'precious Earthen Shielding Jewel',},
+			T{level = 19, name = 'precious Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'precious Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'precious Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'precious Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'precious Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 9
 		T{
-			T{level = 19, name = 'flawless Earthen Essence Jewel',},
-			T{level = 41, name = 'flawless Earthen War Rune Stone',},
-			T{level = 65, name = 'flawless Blood Essence Jewel',},
-			T{level = 84, name = 'flawless Earthern Primal Rune Stone',},
-			T{level = 99, name = 'flawless Earthen Shielding Jewel',},
+			T{level = 19, name = 'flawless Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 41, name = 'flawless Airy War Rune (Archery Skill)',},
+			T{level = 64, name = 'flawless Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'flawless Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'flawless Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 10
 		T{
-			T{level = 19, name = 'perfect Earthen Essence Jewel',},
-			T{level = 41, name = 'perfect Earthen War Rune Stone',},
-			T{level = 65, name = 'perfect Blood Essence Jewel',},
-			T{level = 84, name = 'perfect Earthern Primal Rune Stone',},
-			T{level = 99, name = 'perfect Earthen Shielding Jewel',},
+			T{level = 19, name = 'perfect Earthen Essence Jewel (Constitution Stat)',},
+			T{level = 64, name = 'perfect Airy War Rune (Archery Skill)',},
+			T{level = 65, name = 'perfect Blood Essence Jewel (Hitpoints Stat)',},
+			T{level = 85, name = 'perfect Airy Primal Rune (Battlesongs Skill)',},
+			T{level = 99, name = 'perfect Earthen Shielding Jewel (Matter Resist)',},
 		},
 	},
 	T{	--hib
 		--tier 1
 		T{
-			T{level = 19, name = 'raw Earthen Essence Jewel',},
+			T{level = 19, name = 'raw Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'raw Earthen War Spell Stone',},
-			T{level = 65, name = 'raw Blood Essence Jewel',},
+			T{level = 65, name = 'raw Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'raw Earthen Nature Spell Stone',},
-			T{level = 99, name = 'raw Earthen Shielding Jewel',},
+			T{level = 99, name = 'raw Earthen Shielding Jewel (Matter Resist)',},
 			
 		},
 		--Tier 2
 		T{
-			T{level = 19, name = 'uncut Earthen Essence Jewel',},
+			T{level = 19, name = 'uncut Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'uncut Earthen War Spell Stone',},
-			T{level = 65, name = 'uncut Blood Essence Jewel',},
+			T{level = 65, name = 'uncut Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'uncut Earthen Nature Spell Stone',},
-			T{level = 99, name = 'uncut Earthen Shielding Jewel',},
+			T{level = 99, name = 'uncut Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 3
 		T{
-			T{level = 19, name = 'rough Earthen Essence Jewel',},
+			T{level = 19, name = 'rough Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'rough Earthen War Spell Stone',},
-			T{level = 65, name = 'rough Blood Essence Jewel',},
+			T{level = 65, name = 'rough Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'rough Earthen Nature Spell Stone',},
-			T{level = 99, name = 'rough Earthen Shielding Jewel',},
+			T{level = 99, name = 'rough Earthen Shielding Jewel (Matter Resist)',},
 
 		},
 		--Tier 4
 		T{
-			T{level = 19, name = 'Flawed Earthen Essence Jewel',},
+			T{level = 19, name = 'Flawed Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'Flawed Earthen War Spell Stone',},
-			T{level = 65, name = 'Flawed Blood Essence Jewel',},
+			T{level = 65, name = 'Flawed Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'Flawed Earthen Nature Spell Stone',},
-			T{level = 99, name = 'Flawed Earthen Shielding Jewel',},
+			T{level = 99, name = 'Flawed Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 5
 		T{
-			T{level = 19, name = 'imperfect Earthen Essence Jewel',},
+			T{level = 19, name = 'imperfect Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'imperfect Earthen War Spell Stone',},
-			T{level = 65, name = 'imperfect Blood Essence Jewel',},
+			T{level = 65, name = 'imperfect Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'imperfect Earthen Nature Spell Stone',},
-			T{level = 99, name = 'imperfect Earthen Shielding Jewel',},
+			T{level = 99, name = 'imperfect Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 6
 		T{
-			T{level = 19, name = 'polished Earthen Essence Jewel',},
+			T{level = 19, name = 'polished Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'polished Earthen War Spell Stone',},
-			T{level = 65, name = 'polished Blood Essence Jewel',},
+			T{level = 65, name = 'polished Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'polished Earthen Nature Spell Stone',},
-			T{level = 99, name = 'polished Earthen Shielding Jewel',},
+			T{level = 99, name = 'polished Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 7
 		T{
-			T{level = 19, name = 'faceted Earthen Essence Jewel',},
+			T{level = 19, name = 'faceted Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'faceted Earthen War Spell Stone',},
-			T{level = 65, name = 'faceted Blood Essence Jewel',},
+			T{level = 65, name = 'faceted Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'faceted Earthen Nature Spell Stone',},
-			T{level = 99, name = 'faceted Earthen Shielding Jewel',},
+			T{level = 99, name = 'faceted Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 8
 		T{
-			T{level = 19, name = 'precious Earthen Essence Jewel',},
+			T{level = 19, name = 'precious Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'precious Earthen War Spell Stone',},
-			T{level = 65, name = 'precious Blood Essence Jewel',},
+			T{level = 65, name = 'precious Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'precious Earthen Nature Spell Stone',},
-			T{level = 99, name = 'precious Earthen Shielding Jewel',},
+			T{level = 99, name = 'precious Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 9
 		T{
-			T{level = 19, name = 'flawless Earthen Essence Jewel',},
+			T{level = 19, name = 'flawless Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'flawless Earthen War Spell Stone',},
-			T{level = 65, name = 'flawless Blood Essence Jewel',},
+			T{level = 65, name = 'flawless Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'flawless Earthen Nature Spell Stone',},
-			T{level = 99, name = 'flawless Earthen Shielding Jewel',},
+			T{level = 99, name = 'flawless Earthen Shielding Jewel (Matter Resist)',},
 		},
 		--Tier 10
 		T{
-			T{level = 19, name = 'perfect Earthen Essence Jewel',},
+			T{level = 19, name = 'perfect Earthen Essence Jewel (Constitution Stat)',},
 			T{level = 41, name = 'perfect Earthen War Spell Stone',},
-			T{level = 65, name = 'perfect Blood Essence Jewel',},
+			T{level = 65, name = 'perfect Blood Essence Jewel (Hitpoints Stat)',},
 			T{level = 84, name = 'perfect Earthen Nature Spell Stone',},
-			T{level = 99, name = 'perfect Earthen Shielding Jewel',},
+			T{level = 99, name = 'perfect Earthen Shielding Jewel (Matter Resist)',},
 		},
 	}
 }
